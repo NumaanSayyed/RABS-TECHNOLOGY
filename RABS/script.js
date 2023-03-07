483eabc20d7241adcf4797ef04e1c5c9115741ad
Numan Sayyed <72268126+NumaanSayyed@users.noreply.github.com>
2023-03-07 15:20:10 +0530

    Add files via upload
    Final Commited

---
+++ RABS/script.js
@@ -0,0 +1,29 @@
+// // MDB Lightbox Init
+// // (function () {
+// //     $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+// //   });
+
+// var modal = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+// var btn = document.getElementsByTagName("button")[0];
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal
+// function openModal() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// function closeModal() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
